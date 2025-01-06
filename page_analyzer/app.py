f2e4111a180a0dd604a7e2bde58e1b421fb1c4eb
ilia <iliarassolov@gmail.com>
2025-01-06 22:37:58 +0400

    level_1

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,13 +1,17 @@
 from flask import Flask
+import os
+from dotenv import load_dotenv
 
+
+load_dotenv()
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 
 @app.route('/')
 def index():
-    return '<a href="/">Page analyzer/</a>'
+    return '<a href="/">Page analyzer</a>'
 
-__all__ = ('app',)
 
 if __name__ == '__main__':
     app.run()
