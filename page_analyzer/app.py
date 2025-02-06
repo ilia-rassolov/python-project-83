3c89331b8ce0bbd4195e12cd850ff1229e6a6081
ilia rassolov <iliarassolov@gmail.com>
2025-02-06 12:50:14 +0400

    UrlCRUD

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -2,10 +2,8 @@ from flask import (Flask, redirect, render_template, request,
                    url_for, flash, get_flashed_messages)
 import os
 from dotenv import load_dotenv
-import psycopg2
-from psycopg2.extras import DictCursor
 
-from .db import UrlRepository
+from .db import UrlRepository, UrlCRUD
 from .validator import validate
 
 
@@ -16,7 +14,7 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['DEBUG'] = True
 DATABASE_URL = os.getenv('DATABASE_URL')
 
-conn = psycopg2.connect(DATABASE_URL)
+conn = UrlCRUD(DATABASE_URL)
 
 
 @app.route('/')
@@ -27,19 +25,19 @@ def index():
 
 @app.route('/urls')
 def urls():
-    with conn.cursor(cursor_factory=DictCursor) as cur:
-        repo = UrlRepository(cur)
-        urls = repo.get_content()
-        cur.close()
+    cursor = conn.open_connection()
+    repo = UrlRepository(cursor)
+    urls = repo.get_content()
+    conn.close_connection()
     return render_template('urls.html', urls=urls,)
 
 
 @app.route('/urls/<id>')
 def url_show(id):
-    with conn.cursor(cursor_factory=DictCursor) as cur:
-        repo = UrlRepository(cur)
-        url = repo.find_url(id)
-        cur.close()
+    cursor = conn.open_connection()
+    repo = UrlRepository(cursor)
+    url = repo.find_url(id)
+    conn.close_connection()
     messages = get_flashed_messages(with_categories=True)
     return render_template('show.html', url=url, messages=messages,)
 
@@ -51,18 +49,18 @@ def add_url():
     if errors:
         flash(f"{errors}", 'error')
         return redirect(url_for('index'), code=302)
-    with conn.cursor(cursor_factory=DictCursor) as cur:
-        repo = UrlRepository(cur)
-        id_existing = repo.find_id(url_data)
-        if id_existing:
-            id = id_existing
-            flash('Страница уже существует', 'warning')
-        else:
-            id = repo.save(url_data)
-            conn.commit()
-            flash('Страница успешно добавлена', 'success')
-        url = repo.find_url(id)
-
+    cursor = conn.open_connection()
+    repo = UrlRepository(cursor)
+    id_existing = repo.find_id(url_data)
+    if id_existing:
+        id = id_existing
+        flash('Страница уже существует', 'warning')
+    else:
+        id = repo.save(url_data)
+        conn.commit_db()
+        flash('Страница успешно добавлена', 'success')
+    url = repo.find_url(id)
+    conn.close_connection()
     render_template('show.html', url=url,), 422
     return redirect(url_for('url_show', id=id), code=302)
 
--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -1,40 +1,55 @@
 from urllib.parse import urlparse
 import psycopg2
-import os
-
-
-DATABASE_URL = os.getenv('DATABASE_URL')
-conn = psycopg2.connect(DATABASE_URL)
+from psycopg2.extras import DictCursor
 
 
 class UrlRepository:
-    def __init__(self, cur):
-        self.cur = cur
+    def __init__(self, cursor):
+        self.cursor = cursor
 
     def get_content(self):
-        self.cur.execute("SELECT * FROM urls ORDER BY id DESC")
-        return [dict(row) for row in self.cur]
+        self.cursor.execute("SELECT * FROM urls ORDER BY id DESC")
+        return [dict(row) for row in self.cursor]
 
     def find_url(self, id):
-        self.cur.execute("SELECT * FROM urls WHERE id = %s", (id,))
-        row = self.cur.fetchone()
+        self.cursor.execute("SELECT * FROM urls WHERE id = %s", (id,))
+        row = self.cursor.fetchone()
         return dict(row) if row else None
 
     def save(self, url_data):
         hostname = urlparse(url_data).hostname
-        self.cur.execute(
+        self.cursor.execute(
             """INSERT INTO urls (name) VALUES
             (%s) RETURNING id""",
             (hostname,)
             )
-        id = self.cur.fetchone()[0]
+        id = self.cursor.fetchone()[0]
         return id
 
     def find_id(self, url_data):
         hostname = urlparse(url_data).hostname
-        self.cur.execute("SELECT id, name FROM urls")
-        urls = self.cur.fetchall()
+        self.cursor.execute("SELECT id, name FROM urls")
+        urls = self.cursor.fetchall()
         for row in urls:
             url = dict(row)
             if url["name"] == hostname:
                 return url["id"]
+
+
+class UrlCRUD:
+    def __init__(self, DB_URL):
+        self.db = DB_URL
+
+    def open_connection(self):
+        try:
+            self.conn = psycopg2.connect(self.db)
+        except:
+            print('Can`t establish connection to database')
+        self.cursor = self.conn.cursor(cursor_factory=DictCursor)
+        return self.cursor
+
+    def close_connection(self):
+        return self.cursor.close()
+
+    def commit_db(self):
+        return self.conn.commit()
