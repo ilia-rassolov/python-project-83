473e25a859eff69207f15fd3f1ecfebe75ce4118
ilia rassolov <iliarassolov@gmail.com>
2025-02-06 09:41:19 +0400

    06/02/2025

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -3,6 +3,8 @@ from flask import (Flask, redirect, render_template, request,
 import os
 from dotenv import load_dotenv
 import psycopg2
+from psycopg2.extras import DictCursor
+
 from .db import UrlRepository
 from .validator import validate
 
@@ -14,9 +16,7 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['DEBUG'] = True
 DATABASE_URL = os.getenv('DATABASE_URL')
 
-
 conn = psycopg2.connect(DATABASE_URL)
-repo = UrlRepository(conn)
 
 
 @app.route('/')
@@ -27,13 +27,19 @@ def index():
 
 @app.route('/urls')
 def urls():
-    urls = repo.get_content()
+    with conn.cursor(cursor_factory=DictCursor) as cur:
+        repo = UrlRepository(cur)
+        urls = repo.get_content()
+        cur.close()
     return render_template('urls.html', urls=urls,)
 
 
 @app.route('/urls/<id>')
 def url_show(id):
-    url = repo.find_url(id)
+    with conn.cursor(cursor_factory=DictCursor) as cur:
+        repo = UrlRepository(cur)
+        url = repo.find_url(id)
+        cur.close()
     messages = get_flashed_messages(with_categories=True)
     return render_template('show.html', url=url, messages=messages,)
 
@@ -45,14 +51,18 @@ def add_url():
     if errors:
         flash(f"{errors}", 'error')
         return redirect(url_for('index'), code=302)
-    id_existing = repo.find_id(url_data)
-    if id_existing:
-        id = id_existing
-        flash('Страница уже существует', 'warning')
-    else:
-        id = repo.save(url_data)
-        flash('Страница успешно добавлена', 'success')
-    url = repo.find_url(id)
+    with conn.cursor(cursor_factory=DictCursor) as cur:
+        repo = UrlRepository(cur)
+        id_existing = repo.find_id(url_data)
+        if id_existing:
+            id = id_existing
+            flash('Страница уже существует', 'warning')
+        else:
+            id = repo.save(url_data)
+            conn.commit()
+            flash('Страница успешно добавлена', 'success')
+        url = repo.find_url(id)
+
     render_template('show.html', url=url,), 422
     return redirect(url_for('url_show', id=id), code=302)
 
--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -1,39 +1,40 @@
-from psycopg2.extras import DictCursor
 from urllib.parse import urlparse
+import psycopg2
+import os
+
+
+DATABASE_URL = os.getenv('DATABASE_URL')
+conn = psycopg2.connect(DATABASE_URL)
 
 
 class UrlRepository:
-    def __init__(self, conn):
-        self.conn = conn
+    def __init__(self, cur):
+        self.cur = cur
 
     def get_content(self):
-        with self.conn.cursor(cursor_factory=DictCursor) as cur:
-            cur.execute("SELECT * FROM urls ORDER BY id DESC")
-            return [dict(row) for row in cur]
+        self.cur.execute("SELECT * FROM urls ORDER BY id DESC")
+        return [dict(row) for row in self.cur]
 
     def find_url(self, id):
-        with self.conn.cursor(cursor_factory=DictCursor) as cur:
-            cur.execute("SELECT * FROM urls WHERE id = %s", (id,))
-            row = cur.fetchone()
-            return dict(row) if row else None
+        self.cur.execute("SELECT * FROM urls WHERE id = %s", (id,))
+        row = self.cur.fetchone()
+        return dict(row) if row else None
 
     def save(self, url_data):
         hostname = urlparse(url_data).hostname
-        with self.conn.cursor() as cur:
-            cur.execute(
-                """INSERT INTO urls (name) VALUES
-                (%s) RETURNING id""",
-                (hostname,)
+        self.cur.execute(
+            """INSERT INTO urls (name) VALUES
+            (%s) RETURNING id""",
+            (hostname,)
             )
-            id = cur.fetchone()[0]
-        self.conn.commit()
+        id = self.cur.fetchone()[0]
         return id
 
     def find_id(self, url_data):
         hostname = urlparse(url_data).hostname
-        with self.conn.cursor() as cur:
-            cur.execute("SELECT id, name FROM urls")
-            urls = cur.fetchall()
-            for id, name in urls:
-                if name == hostname:
-                    return id
+        self.cur.execute("SELECT id, name FROM urls")
+        urls = self.cur.fetchall()
+        for row in urls:
+            url = dict(row)
+            if url["name"] == hostname:
+                return url["id"]
