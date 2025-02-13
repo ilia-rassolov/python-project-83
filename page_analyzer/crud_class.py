c771acd5a48cb55f6b87f5b16b7eccc59831751c
ilia <iliarassolov@gmail.com>
2025-02-13 17:03:26 +0400

    13/02/2025

--- page_analyzer/crud_class.py
+++ page_analyzer/crud_class.py
@@ -3,19 +3,16 @@ from psycopg2.extras import DictCursor
 
 
 class CRUD:
-    def __init__(self, DB_URL):
-        self.db = DB_URL
+    def __init__(self, db_url):
+        self.conn = psycopg2.connect(db_url)
 
     def open_connection(self):
-        try:
-            self.conn = psycopg2.connect(self.db)
-        except Exception:
-            print('Can`t establish connection to database')
-        self.cursor = self.conn.cursor(cursor_factory=DictCursor)
-        return self.cursor
-
-    def close_connection(self):
-        return self.cursor.close()
+        self.curs = self.conn.cursor(cursor_factory=DictCursor)
+        return self.curs
 
     def commit_db(self):
         return self.conn.commit()
+
+    def close_connection(self):
+        self.curs.close()
+        self.conn.close()
