44ca1908056b769edf057251c6c5a900c8bc09f7
ilia <iliarassolov@gmail.com>
2025-02-14 11:13:57 +0400

    14/02/2025

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -4,8 +4,8 @@ import os
 from dotenv import load_dotenv
 
 from .db import UrlRepository, CheckRepository
-from .crud_class import CRUD
 from .validator import validate
+from .crud_class import CRUD
 
 
 app = Flask(__name__)
@@ -24,26 +24,24 @@ def index():
 
 @app.route('/urls')
 def urls():
-    conn = CRUD(DATABASE_URL)
-    cursor = conn.open_connection()
-    repo_urls = UrlRepository(cursor)
+    db = CRUD(DATABASE_URL)
+    conn = db.open_connection()
+    repo_urls = UrlRepository(conn)
     content = repo_urls.get_content()
-    cursor.close()
-    conn.close_connection()
+    db.close_connection()
     return render_template('urls.html', content=content,)
 
 
 @app.route('/urls/<id>')
 def url_show(id):
-    conn = CRUD(DATABASE_URL)
-    cursor = conn.open_connection()
-    repo_urls = UrlRepository(cursor)
+    db = CRUD(DATABASE_URL)
+    conn = db.open_connection()
+    repo_urls = UrlRepository(conn)
     url = repo_urls.find_url(id)
 
-    repo_checks = CheckRepository(cursor)
+    repo_checks = CheckRepository(conn)
     checks = repo_checks.get_checks(id)
-    cursor.close()
-    conn.close_connection()
+    db.close_connection()
     messages = get_flashed_messages(with_categories=True)
     return render_template('show.html',
                            url=url, checks=checks, messages=messages,)
@@ -51,44 +49,42 @@ def url_show(id):
 
 @app.post('/')
 def add_url():
-    conn = CRUD(DATABASE_URL)
     url_data = request.form.get("url")
     errors = validate(url_data)
     if errors:
         flash(f"{errors}", 'error')
         return redirect(url_for('index'), code=302)
-    cursor = conn.open_connection()
-    repo_urls = UrlRepository(cursor)
+    db = CRUD(DATABASE_URL)
+    conn = db.open_connection()
+    repo_urls = UrlRepository(conn)
     id_existing = repo_urls.find_id(url_data)
     if id_existing:
         id = id_existing
         flash('Страница уже существует', 'repeat')
     else:
         id = repo_urls.save_url(url_data)
-        conn.commit_db()
         flash('Страница успешно добавлена', 'success')
     url = repo_urls.find_url(id)
-    cursor.close()
-    conn.close_connection()
+    db.commit_db()
+    db.close_connection()
     render_template('show.html', url=url,), 422
     return redirect(url_for('url_show', id=id), code=302)
 
 
 @app.post('/urls/<id>/checks')
 def add_check(id):
-    conn = CRUD(DATABASE_URL)
-    cursor = conn.open_connection()
-    repo_urls = UrlRepository(cursor)
+    db = CRUD(DATABASE_URL)
+    conn = db.open_connection()
+    repo_urls = UrlRepository(conn)
     url = repo_urls.find_url(id)
     new_check = repo_urls.make_check(url)
     if new_check is None:
         flash('Произошла ошибка при проверке', 'error')
         return redirect(url_for('url_show', id=id), code=302)
-    repo_checks = CheckRepository(cursor)
+    repo_checks = CheckRepository(conn)
     repo_checks.save_check(new_check)
-    cursor.close()
-    conn.commit_db()
-    conn.close_connection()
+    db.commit_db()
+    db.close_connection()
     flash('Страница успешно проверена', 'success')
     return redirect(url_for('url_show', id=id), code=302)
 
--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -2,57 +2,65 @@ from urllib.parse import urlparse
 from requests import HTTPError
 import requests
 from bs4 import BeautifulSoup
+from psycopg2.extras import DictCursor
 
 
 class UrlRepository:
-    def __init__(self, cursor):
-        self.cursor = cursor
+    def __init__(self, conn):
+        self.conn = conn
 
     def get_content(self):
-        self.cursor.execute("SELECT id, name FROM urls ORDER BY id DESC")
-        all_urls = [dict(row) for row in self.cursor]
+        with self.conn.cursor(cursor_factory=DictCursor) as curs:
+            curs.execute("SELECT id, name FROM urls ORDER BY id DESC")
+            all_urls = [dict(row) for row in curs]
         content = []
         for url in all_urls:
             row = dict()
-            self.cursor.execute(f"""SELECT
+            with self.conn.cursor(cursor_factory=DictCursor) as curs:
+                curs.execute(f"""SELECT
                                   MAX(id) FROM url_checks
                                   WHERE url_id = {url['id']};""")
-            check_id = self.cursor.fetchone()[0]
+                check_id = curs.fetchone()[0]
             if check_id:
-                self.cursor.execute(f"""SELECT
+                with self.conn.cursor(cursor_factory=DictCursor) as curs:
+                    curs.execute(f"""SELECT
                                      id AS id_check,
                                      created_at AS last_created_at,
                                      status_code AS last_status_code
                                      FROM url_checks WHERE id = {check_id};""")
-                row = dict(self.cursor.fetchone())
+                    row = dict(curs.fetchone())
             row['id_url'] = url['id']
             row['name_url'] = url['name']
             content.append(row)
         return content
 
     def find_url(self, id):
-        self.cursor.execute("SELECT * FROM urls WHERE id = %s", (id,))
-        row = self.cursor.fetchone()
+        with self.conn.cursor(cursor_factory=DictCursor) as curs:
+            curs.execute("SELECT * FROM urls WHERE id = %s", (id,))
+            row = curs.fetchone()
         return dict(row) if row else None
 
     def save_url(self, url_data):
         scheme = urlparse(url_data).scheme
         hostname = urlparse(url_data).hostname
         name = f"{scheme}://{hostname}"
-        self.cursor.execute(
-            """INSERT INTO urls (name) VALUES
+
+        with self.conn.cursor(cursor_factory=DictCursor) as curs:
+            curs.execute("""INSERT INTO urls (name) VALUES
             (%s) RETURNING id""",
-            (name,)
-            )
-        id = self.cursor.fetchone()[0]
+                         (name,)
+                         )
+            id = curs.fetchone()[0]
         return id
 
     def find_id(self, url_data):
         scheme = urlparse(url_data).scheme
         hostname = urlparse(url_data).hostname
         name = f"{scheme}://{hostname}"
-        self.cursor.execute("SELECT id, name FROM urls")
-        urls = self.cursor.fetchall()
+
+        with self.conn.cursor(cursor_factory=DictCursor) as curs:
+            curs.execute("SELECT id, name FROM urls")
+            urls = curs.fetchall()
         for row in urls:
             url = dict(row)
             if url["name"] == name:
@@ -82,17 +90,19 @@ class UrlRepository:
 
 
 class CheckRepository:
-    def __init__(self, cursor):
-        self.cursor = cursor
+    def __init__(self, conn):
+        self.conn = conn
 
     def get_checks(self, url_id):
-        self.cursor.execute("""SELECT
+        with self.conn.cursor(cursor_factory=DictCursor) as curs:
+            curs.execute("""SELECT
                              * FROM url_checks
                              WHERE url_id = %s ORDER BY id DESC""", (url_id,))
-        return [dict(row) for row in self.cursor]
+            return [dict(row) for row in curs]
 
     def save_check(self, new_check):
         placeholders = ', '.join(f'%({k})s' for k in new_check)
         query = (f"""INSERT INTO url_checks
                   ({', '.join(new_check)}) VALUES ({placeholders})""")
-        self.cursor.execute(query, new_check)
+        with self.conn.cursor(cursor_factory=DictCursor) as curs:
+            curs.execute(query, new_check)
